28dc70384463a2f8abc19d1dec1838c7062c8bb8
Andreas Hultgren <andreas.hultgren@frojd.se>
2012-10-21 21:00:26 +0200

    Observer successfully observes basic events and emitts them. Known bug: no event is fired when a song ends

--- js/observer.js
+++ js/observer.js
@@ -5,17 +5,48 @@ function Observer(main){
 		that.main = main;
 		that.player = main.observer;
 		that.socket = main.socket;
-
-		that.state();
+console.log("MJAU");
+		that.observe();
 	}
 
-	Observer.prototype.state = function() {
-		var that = this;
+	Observer.prototype.observe = function() {
+		var that = this,
+			player = that.main.player;
 
 		that.main.player.observe(main.models.EVENT.CHANGE, function(e){
-			if( e.data.playstate ){
-				that.socket.change({state: that.main.player.playing});
+			var changed = {};
+
+			if( e.data.curtrack ){
+				changed.track = player.track.name;
+				changed.album = player.track.album.name;
+				changed.cover = player.track.album.data.cover;
+				changed.uri = player.track.uri;
+				changed.position = 0;
+
+				changed.artists = [];
+
+				player.track.artists.forEach(function(artist){
+					changed.artist.push(artist.name);
+				});
 			}
+
+			if( e.data.playstate ){
+				changed.state = player.playing;
+			}
+
+			if( e.data.volume ){
+				changed.volume = Math.round(player.volume * 100);
+			}
+
+			// Since there's no way to detect change in position, always send it
+			if( changed.position === undefined ){
+				changed.position = player.position;
+			}
+
+			that.socket.change(changed);
 		});
 	};
+
+	var observer = new Observer(main);
+	return observer;
 }
